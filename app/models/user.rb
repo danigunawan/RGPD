52d9cf7bd34b8b9fe983a82c3c6f63e39e011637
Yorick Jacquin <yorickjacquin@gmail.com>
2018-12-27 14:50:16 +0100

    Add the validation of minimum 1 unsub

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :choices
   has_many :unsubscriptions, through: :choices
-  accepts_nested_attributes_for :choices
+  accepts_nested_attributes_for :choices, :unsubscriptions
 
   has_one :request
   has_one :modification, dependent: :destroy
@@ -9,9 +9,25 @@ class User < ApplicationRecord
   before_save :assign_unsubs, :capitalize_name
   before_destroy :destroy_unsub
 
-
+  validate :unsub_must_not_be_blank
   validates :request_id,
   presence: { message: "Vous devez choisir un type de requête." }
+
+
+
+  def unsub_must_not_be_blank
+    if self.request_id == 3
+      pass  = false
+      self.choices.each do |unsub|
+        if unsub.completed == true
+          pass = true
+        end
+      end
+      if pass == false
+        errors.add(:choice, "Vous devez sélectionner au moins un choix")
+      end
+    end
+  end
 
   def assign_unsubs
     Unsubscription.all.each do |unsub|
