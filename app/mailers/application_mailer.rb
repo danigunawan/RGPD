2f62330d0b27e563b3e1f37cb006fb2f28bc6c19
Yorick Jacquin <yorickjacquin@gmail.com>
2019-01-15 16:44:21 +0100

    push some mails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'rgpd@madparis.fr'
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,20 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  # Mailer config
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'mail.madparis.fr',
+    port:                 587,
+    user_name:            ENV['rgpd_username'],
+    password:             ENV['rgpd_password'],
+    authentication:       'plain',
+    enable_starttls_auto: true }
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
