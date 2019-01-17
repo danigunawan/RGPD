76cda9adf5b98309f5c88eb32bef74b0ecd2185d
root <root@rgpd.madparis.fr>
2019-01-17 18:56:45 +0100

    Small change to config for production mails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,70 +1,85 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+	# Settings specified here will take precedence over those in config/application.rb.
 
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+	# In the development environment your application's code is reloaded on
+	# every request. This slows down response time but is perfect for development
+	# since you don't have to restart the web server when you make code changes.
+	config.cache_classes = false
 
-  # Do not eager load code on boot.
-  config.eager_load = false
+	# Mailer config
+	#   # Don't care if the mailer can't send.
+	config.action_mailer.raise_delivery_errors = true
+	config.action_mailer.perform_deliveries = true
+	config.action_mailer.perform_caching = false
+	config.action_mailer.delivery_method = :smtp
+	config.action_mailer.smtp_settings = {
+		address:              'mail.madparis.fr',
+		port:                 587,
+		user_name:            ENV['rgpd_username'],
+		password:             ENV['rgpd_password'],
+		authentication:       'plain',
+		enable_starttls_auto: true }
 
-  # Show full error reports.
-  config.consider_all_requests_local = true
 
-  # Enable/disable caching. By default caching is disabled.
-  # Run rails dev:cache to toggle caching.
-  if Rails.root.join('tmp', 'caching-dev.txt').exist?
-    config.action_controller.perform_caching = true
+	# Do not eager load code on boot.
+	config.eager_load = false
 
-    config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
-    }
-  else
-    config.action_controller.perform_caching = false
+	# Show full error reports.
+	config.consider_all_requests_local = true
 
-    config.cache_store = :null_store
-  end
+	# Enable/disable caching. By default caching is disabled.
+	# Run rails dev:cache to toggle caching.
+	if Rails.root.join('tmp', 'caching-dev.txt').exist?
+		config.action_controller.perform_caching = true
 
-  # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
-  config.action_controller.action_on_unpermitted_parameters = :raise
+		config.cache_store = :memory_store
+		config.public_file_server.headers = {
+			'Cache-Control' => "public, max-age=#{2.days.to_i}"
+		}
+	else
+		config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.perform_caching = false
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'mail.madparis.fr',
-    port:                 587,
-    user_name:            ENV['rgpd_test_username'],
-    password:             ENV['rgpd_test_password'],
-    authentication:       'plain',
-    enable_starttls_auto: true }
+		config.cache_store = :null_store
+	end
 
-  # Print deprecation notices to the Rails logger.
-  config.active_support.deprecation = :log
+	# Store uploaded files on the local file system (see config/storage.yml for options)
+	config.active_storage.service = :local
+	config.action_controller.action_on_unpermitted_parameters = :raise
 
-  # Raise an error on page load if there are pending migrations.
-  config.active_record.migration_error = :page_load
+	# Don't care if the mailer can't send.
+	config.action_mailer.raise_delivery_errors = true
+	config.action_mailer.perform_deliveries = true
+	config.action_mailer.perform_caching = false
+	config.action_mailer.delivery_method = :smtp
+	config.action_mailer.smtp_settings = {
+		address:              'mail.madparis.fr',
+		port:                 587,
+		user_name:            ENV['rgpd_test_username'],
+		password:             ENV['rgpd_test_password'],
+		authentication:       'plain',
+		enable_starttls_auto: true }
 
-  # Highlight code that triggered database queries in logs.
-  config.active_record.verbose_query_logs = true
+	# Print deprecation notices to the Rails logger.
+	config.active_support.deprecation = :log
 
-  # Debug mode disables concatenation and preprocessing of assets.
-  # This option may cause significant delays in view rendering with a large
-  # number of complex assets.
-  config.assets.debug = true
+	# Raise an error on page load if there are pending migrations.
+	config.active_record.migration_error = :page_load
 
-  # Suppress logger output for asset requests.
-  config.assets.quiet = true
+	# Highlight code that triggered database queries in logs.
+	config.active_record.verbose_query_logs = true
 
-  # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
+	# Debug mode disables concatenation and preprocessing of assets.
+	# This option may cause significant delays in view rendering with a large
+	# number of complex assets.
+	config.assets.debug = true
 
-  # Use an evented file watcher to asynchronously detect changes in source code,
-  # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+	# Suppress logger output for asset requests.
+	config.assets.quiet = true
+
+	# Raises error for missing translations
+	# config.action_view.raise_on_missing_translations = true
+
+	# Use an evented file watcher to asynchronously detect changes in source code,
+	# routes, locales, etc. This feature depends on the listen gem.
+	config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
