703a2327b524fda2c800a49ff84c95c84527cde2
Yorick Jacquin <yorickjacquin@gmail.com>
2019-01-17 17:04:54 +0100

    Fix bug with mail delivery

--- app/mailers/officer_mailer.rb
+++ app/mailers/officer_mailer.rb
@@ -3,28 +3,28 @@ class OfficerMailer < ApplicationMailer
 
   def access_right
     @user = params[:user]
-    Officer.each do |officer|
+    Officer.all.each do |officer|
       mail(to: officer.mail, subject: "[FORMULAIRE-RGPD] Nouvelle demande de droit d'accès")
     end
   end
 
   def edit_right
     @user = params[:user]
-    Officer.each do |officer|
+    Officer.all.each do |officer|
       mail(to: officer.mail, subject: "[FORMULAIRE-RGPD] Nouvelle demande de rectification")
     end
   end
 
   def limit_right
     @user = params[:user]
-    Officer.each do |officer|
+    Officer.all.each do |officer|
       mail(to: officer.mail, subject: "[FORMULAIRE-RGPD] Nouvelle demande de limitation de traitements")
     end
   end
 
   def delete_right
     @user = params[:user]
-    Officer.each do |officer|
+    Officer.all.each do |officer|
       mail(to: officer.mail, subject: "[FORMULAIRE-RGPD] Nouvelle demande d'effacement'")
     end
   end
